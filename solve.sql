673f430ebebbb9ac4695a6fd3a33dcbc06851ddc
Ashraful Talukder <137375997+GrimReaper0070@users.noreply.github.com>
2026-07-13 22:14:03 +0600

    Update solve.sql

--- solve.sql
+++ solve.sql
@@ -4,3 +4,9 @@ WHERE COUNTRYCODE = 'USA'
     AND POPULATION > 100000;
 
 Query all columns for all American cities in the CITY table with populations larger than 100000. The CountryCode for America is USA.
+
+
+SELECT NAME
+FROM CITY
+WHERE COUNTRYCODE = 'USA'
+AND POPULATION > 120000;
